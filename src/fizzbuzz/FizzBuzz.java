c65bbd2e9ea2623122f19a812108a29578e31896
Kylian-BBO <kylian.barbero@gmail.com>
2023-04-20 16:52:01 +0200

    fizzBuzz(3) return fizz

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,9 @@ package fizzbuzz;
 
 public class FizzBuzz {
 	
-	public String fizzBuzz(int i) {
-		return Integer.toString(i);
+	public String fizzBuzz(int numero) {
+		if (numero == 3)
+			return "fizz";
+		return Integer.toString(numero);
 	}
 }
