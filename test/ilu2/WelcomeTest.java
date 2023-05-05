937091b8352af5a0ab7e6feb1ddd950f349ca10b
Kylian-BBO <kylian.barbero@gmail.com>
2023-05-05 19:25:22 +0200

    ITER_8 - Tests enrichis

--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -28,22 +28,26 @@ class WelcomeTest {
 	
 	@Test
 	void ITER_5() {
-		assertEquals("Hello, Amy, Bob and Jerry", Welcome.welcome("Amy, bob,jerry"));
+		assertEquals("Hello, Amy, Bob and Jerry", Welcome.welcome("Amy,bob,jerry"));
 	}
 	
 	@Test
 	void ITER_6() {
 		assertEquals("Hello, Amy and Jerry. AND HELLO, BOB !", Welcome.welcome("Amy, BOB, Jerry"));
+		assertEquals("HELLO, JERRY, AMY AND BOB !", Welcome.welcome("JERRY,AMY,BOB"));
 	}
 	
 	@Test
 	void ITER_7() {
 		assertEquals("Hello, Bob, Amy and Jerry", Welcome.welcome("bob, amy, jerry"));
-		assertEquals("Hello, Bob and Jerry. AND HELLO, AMY AND JACK !", Welcome.welcome("bob, AMY, jerry, JACK"));		
+		assertEquals("Hello, Bob and Jerry. AND HELLO, AMY AND JACK !", Welcome.welcome("bob, AMY, jerry, JACK"));
+		assertEquals("HELLO, AMY AND JACK !", Welcome.welcome("AMY,JACK"));
 	}
 	
 	@Test
 	void ITER_8() {
-		assertEquals("Hello, Bob and Amy", Welcome.welcome("bob    , amy   "));		
+		assertEquals("Hello, Bob and Amy", Welcome.welcome("bob    , amy   "));
+		assertEquals("HELLO, AMY AND BOB !", Welcome.welcome(" AMY  ,    BOB "));
+		assertEquals("Hello, Jack. AND HELLO, AMY AND BOB !", Welcome.welcome("    jack, AMY  ,    BOB "));	
 	}
 }
