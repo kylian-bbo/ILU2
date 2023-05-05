4fc01752a307ea3f74f86d060d380e0d0fe5489b
Kylian-BBO <kylian.barbero@gmail.com>
2023-05-05 19:05:41 +0200

    ITER_7

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -23,8 +23,13 @@ public class Welcome {
 	private static void gestionOutputMin(String[] noms, StringBuilder output) {
 		for (int i=0; i<noms.length; i++) {
 			if (!(noms[i].equals(noms[i].toUpperCase()))) {
-				if (i>0)
-					output.append(", ");
+				if (i>0) {
+					if ((estDernierMin(noms[i], noms)) && (!estPremierMin(noms[i], noms)))
+						output.append(" and ");
+					else
+						output.append(", ");
+				}
+				
 				if (i==0)
 					output.append("Hello, ");
 				
@@ -45,8 +50,12 @@ public class Welcome {
 						output.append(". AND ");
 					output.append("HELLO, ");
 				}
-				if (numNomMaj>1)
-					output.append(", ");
+				if (numNomMaj>1) {
+					if ((estDernierMaj(noms[i], noms)) && (!estPremierMaj(noms[i], noms)))
+						output.append(" AND ");
+					else
+						output.append(", ");
+				}
 			
 				output.append(formatNom(noms[i]));
 				
@@ -60,8 +69,57 @@ public class Welcome {
 		
 	private static String formatNom(String nomTemp) {
 		String nom = nomTemp.trim();
+		
 		if (nom.equals(nom.toUpperCase()))
 			return nom;
 		return nom.substring(0,1).toUpperCase() + nom.substring(1);
+	}
+	
+	private static Boolean estDernierMin(String nom, String[] noms) {
+		String dernierMin = null;
+		
+		for (int i=0; i<noms.length; i++) {
+			if (!noms[i].equals(noms[i].toUpperCase())) {
+				dernierMin = noms[i];
+			}
+		}
+		
+		return nom.equals(dernierMin);
+	}
+	
+	private static Boolean estDernierMaj(String nom, String[] noms) {
+		String dernierMaj = null;
+		
+		for (int i=0; i<noms.length; i++) {
+			if (noms[i].equals(noms[i].toUpperCase())) {
+				dernierMaj = noms[i];
+			}
+		}
+		
+		return nom.equals(dernierMaj);
+	}
+	
+	private static Boolean estPremierMin(String nom, String[] noms) {
+		String premierMin = null;
+		
+		for (int i=0; i<noms.length && premierMin == null; i++) {
+			if (!noms[i].equals(noms[i].toUpperCase())) {
+				premierMin = noms[i];
+				return nom.equals(premierMin);
+			}
+		}
+		return false;
+	}
+	
+	private static Boolean estPremierMaj(String nom, String[] noms) {
+		String premierMaj = null;
+		
+		for (int i=0; i<noms.length && premierMaj == null; i++) {
+			if (noms[i].equals(noms[i].toUpperCase())) {
+				premierMaj = noms[i];
+				return nom.equals(premierMaj);
+			}
+		}
+		return false;
 	}
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -24,16 +24,22 @@ class WelcomeTest {
 	
 	@Test
 	void ITER_4() {
-		assertEquals("Hello, Amy, Bob", Welcome.welcome("amy,bob"));
+		assertEquals("Hello, Amy and Bob", Welcome.welcome("amy,bob"));
 	}
 	
 	@Test
 	void ITER_5() {
-		assertEquals("Hello, Amy, Bob, Jerry", Welcome.welcome("Amy, bob,jerry"));
+		assertEquals("Hello, Amy, Bob and Jerry", Welcome.welcome("Amy, bob,jerry"));
 	}
 	
 	@Test
 	void ITER_6() {
-		assertEquals("Hello, Amy, Jerry. AND HELLO, BOB !", Welcome.welcome("Amy, BOB, Jerry"));
+		assertEquals("Hello, Amy and Jerry. AND HELLO, BOB !", Welcome.welcome("Amy, BOB, Jerry"));
+	}
+	
+	@Test
+	void ITER_7() {
+		assertEquals("Hello, Bob, Amy and Jerry", Welcome.welcome("bob, amy, jerry"));
+		assertEquals("Hello, Bob and Jerry. AND HELLO, AMY AND JACK !", Welcome.welcome("bob, AMY, jerry, JACK"));		
 	}
 }
